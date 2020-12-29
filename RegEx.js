07553b278c30bc48690b65b045764394ccd75064
MURA199918 <muralisaranr18@gmail.com>
2020-12-29 11:53:53 +0530

    [Murali]Refactored to restrict PIN Code from taking alphabets or special characters at beginning

--- RegEx.js
+++ RegEx.js
@@ -1,9 +1,23 @@
 console.log("Welcome to validation using RegEx.");
 
-let regEx_PINCODE = RegExp('^[1-9]{1}[0-9]{5}$');
-var pincode1 = 400076;
-if(regEx_PINCODE.test(pincode1)){
-    console.log("Pincode validated successfully");
-}else{
-    console.log("Incorrect Validation");
+//UC1 - PINCODE
+{
+        let regEx_PINCODE = RegExp('^[1-9]{1}[0-9]{5}$');
+        var pincode1 = 400076;
+        if(regEx_PINCODE.test(pincode1)){
+            console.log("UC1 - Pincode validated successfully");
+        }else{
+            console.log("UC1 - Incorrect Validation");
+        }
+}
+
+//UC2 - PINCODE
+{
+    let regEx_PINCODE = RegExp('^[1-9]{1}[0-9]{6}$');
+    var pincode1 = 'A400076';
+    if(regEx_PINCODE.test(pincode1)){
+        console.log("UC2 - Pincode validated successfully");
+    }else{
+        console.log("UC2 - Incorrect Validation");
+    }
 }
